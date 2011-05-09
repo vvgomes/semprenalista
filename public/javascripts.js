67df290ea02a9984a9f1de207eb6260a3d53e53f
Vinicius Gomes <vvgomess@gmail.com>
2011-05-09 13:08:49 -0300

    html 5 stuff

--- public/javascripts.js
+++ public/javascripts.js
@@ -1,16 +1,1 @@
-$(document).ready(function() {	
-		
-	$('input[name=name]').focus();
-	
-	$('form').validate({ 
-		rules: { 
-   		name: 'required',
-      email: 'required' 
-    }, 
-    messages: { 
-      name: '*',
- 			email: '*'
-    } 
-  });
-
-});
+document.getElementsByName('name')[0].focus();
