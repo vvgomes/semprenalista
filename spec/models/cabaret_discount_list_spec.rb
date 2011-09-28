81cc278d1297ca0d204e0a1368c61c97479b704b
Vinicius Gomes <vvgomess@gmail.com>
2011-09-28 20:40:10 -0300

    some code cleanup

--- spec/models/cabaret_discount_list_spec.rb
+++ spec/models/cabaret_discount_list_spec.rb
@@ -2,53 +2,63 @@ require File.expand_path(File.dirname(__FILE__) + '/../../app/models/cabaret')
 
 describe 'Cabaret::DiscountList' do
 
-  before :each do
-    @page = mock
+  context 'that does not have a form' do
+
+    it 'should not be able to add a nightclubber to the list' do
+      amnesia_list = Cabaret::DiscountList.new page_without_form
+      amnesia_list.add(sabella).should be_an_instance_of Cabaret::EmptyResponse
+    end
+
+    def page_without_form
+      page_with nil
+    end
+
   end
 
   context 'that has a form' do
 
     before :each do
-      @form = mock
-      @page.should_receive(:form_with).with(:action => /cadastra.php/i).and_return @form
-      @amnesia_list = Cabaret::DiscountList.new @page
+      agent = mock
+      agent.stub!(:submit).and_return mock
+      Mechanize.stub!(:new).and_return agent
     end
 
-    it 'should be nice' do
-      @amnesia_list.should be_nice
+    it 'should fill the form with nightclubber data' do
+      friend_input = mock
+      friend_input.should_receive(:value=).with 'Marano'
+
+      form = mock
+      form.should_receive(:[]=).once.with 'name','Filipe Sabella'
+      form.should_receive(:[]=).once.with 'email','sabella@gmail.com'
+      form.should_receive(:fields_with).with(:name => /amigo/).and_return [friend_input]
+
+      Cabaret::DiscountList.new(page_with(form)).add sabella
     end
 
     it 'should give a response back after adding a nightclubber' do
-      agent = mock
-      agent.should_receive(:submit).with(@form).and_return mock
-      Mechanize.stub!(:new).and_return agent
+      amnesia_list = Cabaret::DiscountList.new(page_with(dumb_form))
+      amnesia_list.add(sabella).should be_an_instance_of Cabaret::Response
+    end
 
-      friend_field = mock
-      friend_field.should_receive(:value=).with 'Marano'
-      @form.should_receive(:[]=).once.with 'name','Filipe Sabella'
-      @form.should_receive(:[]=).once.with 'email','sabella@gmail.com'
-      @form.should_receive(:fields_with).with(:name => /amigo/).and_return [friend_field]
-      response = @amnesia_list.add sabella
-      response.should be_an_instance_of Cabaret::Response
+    def dumb_form
+      friend_input = mock
+      friend_input.stub!(:value=)
+
+      form = mock
+      form.stub!(:[]=)
+      form.stub!(:fields_with).
+      and_return [friend_input]
+
+      form
     end
 
   end
 
-  context 'that does not have a form' do
-
-    before :each do
-      @page.should_receive(:form_with).with(:action => /cadastra.php/i).and_return nil
-      @amnesia_list = Cabaret::DiscountList.new @page
-    end
-
-    it 'should not be nice' do
-      @amnesia_list.should_not be_nice
-    end
-
-    it 'should not be able to add a nightclubber to the list' do
-      @amnesia_list.add(sabella).should be_an_instance_of Cabaret::EmptyResponse
-    end
-
+  def page_with form
+    page = mock
+    page.should_receive(:form_with).
+    with(:action => /cadastra.php/i).and_return form
+    page
   end
 
   def sabella
@@ -58,5 +68,6 @@ describe 'Cabaret::DiscountList' do
     sabella.stub!(:friends).and_return ['Marano']
     sabella
   end
+
 end
 
