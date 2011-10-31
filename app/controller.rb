8fe867630bd50b78bd496ebb961199de914415da
Vinicius Gomes <vvgomess@gmail.com>
2011-10-31 11:41:03 -0200

    working on #3

--- app/controller.rb
+++ app/controller.rb
@@ -69,4 +69,3 @@ get '/reports' do
     :reports => reports
   }
 end
-
--- public/javascripts.js
+++ public/javascripts.js
@@ -1,4 +1,6 @@
 function showDialog() {
-
+	var dialog = $dom.get('#dialog');
+	dialog['class'] = '';
+	console.log(dialog)
 }
 
