dd664c06938c739941be4d8589943ccf80509276
Vinicius Gomes <vvgomess@gmail.com>
2012-01-02 14:49:34 -0200

    small changes to views

--- app/controller.rb
+++ app/controller.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../config/environment')
 configure do
   enable :sessions
   set :app_file, __FILE__
-  set :public, File.dirname(__FILE__)+'/../public'
+  set :public_folder, File.dirname(__FILE__)+'/../public'
   set :views, File.dirname(__FILE__)+'/views'
 end
 
