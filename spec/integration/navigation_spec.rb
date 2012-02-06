5c88241d54d34a0e5124f35b1571052101c1eafa
Vinicius Gomes <vvgomess@gmail.com>
2012-02-06 01:00:16 -0200

    casadolado is back :)

--- spec/integration/navigation_spec.rb
+++ spec/integration/navigation_spec.rb
@@ -17,7 +17,6 @@ describe 'Navigators' do
   end
   
   it 'should reflect the web site structure for Casadolado' do
-    pending 'casa do lado is down again'
     browse Nightclub.new(Casadolado::Navigator.new)
   end
 
