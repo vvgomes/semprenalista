5ba9eb5f832d7e29fee53d4cb427f5bdbf4108c6
Vinicius Gomes <vvgomess@gmail.com>
2012-02-09 21:35:31 -0200

    removing the selenium thing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,3 @@ require File.expand_path(File.dirname(__FILE__) + '/../config/environment')
 require File.expand_path(File.dirname(__FILE__) + '/../app/controller')
 require 'capybara/rspec'
 Capybara.app = Sinatra::Application
-Capybara.default_driver = :selenium
