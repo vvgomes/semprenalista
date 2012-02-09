aa5721b745257aac0adbaba24051911e3c4c62f3
Pedro Nascimento <pnascimento@gmail.com>
2012-02-09 17:30:56 -0200

    Configuring capybara and moving some tests to it. Using RackTest instead of Selenium, we do not have anything that needs Selenium yet.

--- spec/requests/new_nightclubber_spec.rb
+++ spec/requests/new_nightclubber_spec.rb
@@ -1,87 +1,71 @@
-require 'selenium-webdriver'
-
-describe 'A new nightclubber' do
-  HOME = 'http://127.0.0.1:9292'
-  
+#encoding: utf-8
+require 'spec_helper'
+describe 'A new nightclubber', :type => :request do
   before :all do
-    @driver = Selenium::WebDriver.for :firefox
+    Nightclubber.delete_all
   end
-  
-  after :all do
-    @driver.quit
-  end
-  
-  context 'when joing with valid information' do 
-    
+  context 'when joining with valid information' do
+
     before :all do
-      @driver.navigate.to HOME
-      @driver.find_element(:name, 'name').send_keys 'Silvio Santos'
-      @driver.find_element(:name, 'email').send_keys 'silvio@sbt.com.br'
-      @driver.find_element(:name, 'friends[0]').send_keys 'Lombardi'
-      @driver.find_element(:name, 'friends[1]').send_keys 'Roque'
-      @driver.find_element(:name, 'friends[2]').send_keys 'Pedro de Lara'
-      @driver.find_element(:name, 'friends[3]').send_keys 'Celso Portioli'
-      @driver.find_element(:id, 'ok').click
+      visit '/'
+      fill_in 'name', :with => 'Silvio Santos'
+      fill_in 'email', :with => 'silvio@sbt.com.br'
+      fill_in 'friends[0]', :with => 'Lombardi'
+      fill_in 'friends[1]', :with => 'Roque'
+      fill_in 'friends[2]', :with => 'Pedro de Lara'
+      fill_in 'friends[3]', :with => 'Celso Portioli'
+      click_button 'OK'
     end
 
     after :all do
       Nightclubber.where(:email => 'silvio@sbt.com.br').delete
     end
-    
+
     it 'should be redirected to /done' do
-      @driver.current_url.should be == "#{HOME}/done"
+      page.current_url.should =~ /done/
     end
-    
-    it 'should be listed in /nightclubbers' do
-      @driver.navigate.to "#{HOME}/nightclubbers"
-      @driver.page_source.should include 'Silvio Santos'
+
+    context 'listing in /nightclubbers' do
+      before do
+        visit '/nightclubbers'
+      end
+
+      it 'should list the nightclubber' do
+        page.should have_content 'Silvio Santos'
+      end
+
+      it 'should list nighclubbers frineds' do
+        page.should have_content 'Lombardi'
+        page.should have_content 'Roque'
+        page.should have_content 'Pedro de Lara'
+        page.should have_content 'Celso Portioli'
+      end
     end
-    
-    it 'should have his friends listed in /nightclubbers' do
-      @driver.navigate.to "#{HOME}/nightclubbers"
-      @driver.page_source.should include 'Lombardi'
-      @driver.page_source.should include 'Roque'
-      @driver.page_source.should include 'Pedro de Lara'
-      @driver.page_source.should include 'Celso Portioli'
-    end
-    
   end
-  
-  context 'when joing without his name' do
-    
-    before :all do
-      @driver.navigate.to HOME
-      @driver.find_element(:name, 'email').send_keys 'silvio@sbt.com.br'
-      @driver.find_element(:id, 'ok').click
+
+  context 'when joining, do not add the nightclubber if' do
+    before do
+      visit '/'
     end
-    
-    it 'should stay at /' do
-      @driver.current_url.should be == "#{HOME}/"
+    pending 'has a name missing' do
+      fill_in 'email', :with => 'silvio@sbt.com.br'
     end
-    
+
+    pending 'has an email missing' do
+      fill_in 'name', :with => 'Silvio Santos'
+    end
+
+    after do
+      click_button 'OK'
+      page.current_url.should_not =~ /done/
+      visit '/nightclubbers'
+      page.should have_no_content 'Silvio Santos'
+    end
   end
-    
-  context 'when joing without his email' do
-    
-    before :all do
-      @driver.navigate.to HOME
-      @driver.find_element(:name, 'name').send_keys 'Silvio Santos'
-      @driver.find_element(:id, 'ok').click
-    end
-    
-    it 'should stay at /' do
-      @driver.current_url.should be == "#{HOME}/"
-    end
-    
-    it 'should not be listed at /nightclubbers' do
-      @driver.navigate.to "#{HOME}/nightclubbers"
-      @driver.page_source.should_not include 'Silvio Santos'
-    end
-    
-  end
-  
+
+=begin
   context 'when joing again' do
-    
+
     before :all do
       2.times do
         @driver.navigate.to HOME
@@ -90,20 +74,20 @@ describe 'A new nightclubber' do
         @driver.find_element(:id, 'ok').click
       end
     end
-    
+
     after :all do
       Nightclubber.where(:email => 'silvio@sbt.com.br').delete
     end
-    
+
     it 'should stay at /' do
       @driver.current_url.should be == "#{HOME}/"
     end
-    
+
     it 'should see the error message' do
       message = @driver.find_element(:class, 'error').text
       message.should be == 'Email já cadastrado :('
     end
-    
-  end
-    
+
+=end
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,4 @@
 require File.expand_path(File.dirname(__FILE__) + '/../config/environment')
+require File.expand_path(File.dirname(__FILE__) + '/../app/controller')
+require 'capybara/rspec'
+Capybara.app = Sinatra::Application
