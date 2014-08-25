c60cfa251a60422d6f8100e8fec2f70911e761bb
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2014-08-25 16:39:43 -0300

    sessions controller spec

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,6 +1,34 @@
 require 'rails_helper'
 
 describe SessionsController, :type => :controller do
-  describe 'POST #create' do
+
+  describe '#create' do
+    let!(:dude) do
+      create(:user, :provider => 'facebook', :uid => '666')
+    end
+
+    let(:auth) do
+      OmniAuth::AuthHash.new({
+        :provider => 'facebook', 
+        :uid => '666',
+        :info => {},
+        :credentials => {
+          :expires_at => Time.now.to_i
+        }
+      })
+    end
+
+    before do
+      allow_any_instance_of(SessionsController).
+        to receive(:env).and_return({ 'omniauth.auth' => auth })
+    end
+
+    before do
+      get :create, :provider => 'facebook'
+    end
+
+    specify { expect(response).to have_http_status(302) }
+    specify { expect(response).to redirect_to root_url }
+    specify { expect(session[:user_id]).to eq(dude.id) }
   end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -2,10 +2,8 @@ FactoryGirl.define do
   factory :user do
     name 'Dude'
     email 'dude@gmail.com'
-    image 'image.jpg'
+    uid '666'
     provider 'facebook'
-    uid '123456'
-    access_token 'AAbbCC'
   end
 end
 
