756faaef0dcde7031c9c9554646d955e5b8acaaa
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2014-10-03 11:44:56 -0300

    makes athentication available to all controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user
+  helper_method :current_user, :authenticate!
 
   def index
   end
